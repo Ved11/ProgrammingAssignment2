9c554c7c1d313aa5c2b96598065f99f1b942c513
Ved <vedfolnir@hotmail.fr>
2017-05-01 21:59:59 +0200

    Programming assignment 2 solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { #I create the function containing the 4 functions as for the makeVector
+      i <- NULL                                 #Empty i object will contain the inverted matrix      
+      set <- function(y) {                      #Set function
+            x <<- y
+            i <<- NULL
+      }
+      get <- function() x                       #Get function
+      setinv <- function(solve) i <<- solve     #setinv function
+      getinv <- function() i                    #getinv
+      list(set = set, get = get,                #List stores the 4 functions
+           setinv = setinv,
+           getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should 
+## retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+      i <- x$getinv()                           #Check if there is inv stored
+      if(!is.null(i)) {                         #If x$getinv is not null...
+            message("getting cached data")      #Then it means we get the cached data...
+            return(i)                           #And return it
+      }
+      data <- x$get()                           #if null..
+      i <- solve(data, ...)                     #We make it happen, using the solve function on the matrix
+      x$setinv(i)                               
+      i        # And finally return a matrix that is the inverse of 'x'
 }
